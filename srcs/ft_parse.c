2247da95d0a732b215c5430d22a2e1a3c32db7b4
Steve-alan BALEBA BALEBA <stbaleba@e3r12p6.42.fr>
2019-11-12 16:09:06 +0100

    rm parsing file

--- srcs/ft_parse.c
+++ srcs/ft_parse.c
@@ -6,7 +6,7 @@
 /*   By: blacking <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 12:21:28 by blacking          #+#    #+#             */
-/*   Updated: 2019/11/12 15:54:46 by stbaleba         ###   ########.fr       */
+/*   Updated: 2019/11/12 16:06:32 by stbaleba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,23 @@ void	ft_parsing_flags(t_printf *params, int *count)
 	if(params->minus == 1)
 		width(params, count);
 	free(params);
+}
+
+void	parse(t_printf *params, int *count)
+{
+	if(params->type == 'd' || params->type == 'i'
+	|| params->type == 'u')
+		ft_putnbr(params->var_int, count);
+	else if(params->type == 'c')
+		ft_putchar_int(params->var_int, count);
+	else if(params->type == 's' && params->dot == 0)
+		ft_putstr(params->var_string, count);
+	else if(params->type == 'p')
+		ft_putnbr_hexa(params->var_unslong, count, params);
+	else if(params->type == 'x' || params->type == 'X')
+		ft_putnbr_hexa(params->var_unsint, count, params);
+	else if(params->type == '%')
+		ft_putchar_int('%', count);
 }
 
 void	ft_fill_struct(const char **str, int *count, va_list ap)
